b6cee1068abc10f8db13060cea279a72a9ae527e
Islam Shamurov <shamurovislam8@gmail.com>
2022-06-10 10:33:57 +0500

    first commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import '../utils/Strings.dart';
 import 'home_assignment.dart';
 
-
-
 class HomeAssignmentFour extends StatefulWidget {
   static const String id = '/home_assignment_four';
 
@@ -35,7 +33,7 @@ class _HomeAssignmentFourState extends State<HomeAssignmentFour> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.only(top: 20,right: 20,left: 20),
+        padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
         child: Stack(
           alignment: Alignment.bottomRight,
           children: [
@@ -51,20 +49,17 @@ class _HomeAssignmentFourState extends State<HomeAssignmentFour> {
                     image: 'assets/images/img_6.png',
                     title: Strings.stepOneTitle,
                     content: Strings.stepOneContent,
-                    toShow: false
-                ),
+                    toShow: false),
                 makePage(
                     image: 'assets/images/img_7.png',
                     title: Strings.stepTwoTitle,
                     content: Strings.stepTwoContent,
-                    toShow: false
-                ),
+                    toShow: false),
                 makePage(
                     image: 'assets/images/img_8.png',
                     title: Strings.stepThreeTitle,
                     content: Strings.stepThreeContent,
-                    toShow: true
-                ),
+                    toShow: true),
               ],
             ),
             Container(
@@ -81,67 +76,69 @@ class _HomeAssignmentFourState extends State<HomeAssignmentFour> {
   }
 
   Widget makePage({image, title, content, toShow}) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              color: Colors.redAccent,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.redAccent,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
           ),
-          SizedBox(height: 25,),
-          Text(
-            content,
-            style: TextStyle(
-              color: Colors.grey.shade400,
-              fontSize: 20,
-            ),
-            textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Text(
+          content,
+          style: TextStyle(
+            color: Colors.grey.shade400,
+            fontSize: 20,
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          Image(
-            image: AssetImage(image),
-          ),
-          const SizedBox(
-            height: 75,
-          ),
-          toShow ?   Row(
-
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(
-                    context,
-                    FourHomeAssignment.id,
-                  );
-                },
-                child: const Text(
-                  'Skip',
-                  style: TextStyle(color: Colors.redAccent, fontSize: 20),
-                ),
-              ),
-            ],
-          ) : const SizedBox.shrink(),
-          const SizedBox(
-            height: 30,
-          ),
-        ],
-      );
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Image(
+          image: AssetImage(image),
+        ),
+        const SizedBox(
+          height: 75,
+        ),
+        toShow
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                        context,
+                        FourHomeAssignment.id,
+                      );
+                    },
+                    child: const Text(
+                      'Skip',
+                      style: TextStyle(color: Colors.redAccent, fontSize: 20),
+                    ),
+                  ),
+                ],
+              )
+            : const SizedBox.shrink(),
+        const SizedBox(
+          height: 30,
+        ),
+      ],
+    );
   }
 
   Widget pageRunner(bool isTrue) {
     return AnimatedContainer(
-
       duration: const Duration(milliseconds: 200),
       width: isTrue ? 30 : 6,
       height: 5,
-      margin: const EdgeInsets.only(left: 10,bottom: 20),
+      margin: const EdgeInsets.only(left: 10, bottom: 20),
       decoration: BoxDecoration(
         color: Colors.redAccent,
         borderRadius: BorderRadius.circular(10),
@@ -165,4 +162,3 @@ class _HomeAssignmentFourState extends State<HomeAssignmentFour> {
     return result;
   }
 }
-
